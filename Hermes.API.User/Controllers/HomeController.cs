d1df54499bef193b93f38e253e1bd8fc07d1a3c3
Berkay YALCIN <yalcinberkaycom@gmail.com>
2021-03-10 00:16:19 +0300

    Add User API

---
+++ Hermes.API.User/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Hermes.API.User.Controllers
+{
+    [ApiController]
+    [Route("v1/[controller]")]
+    public class HomeController : ControllerBase
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            return Ok("User API Works");
+        }
+    }
+}
