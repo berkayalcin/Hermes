4b7eb43c2d18b8806aedda70d7c4989a34caa892
Berkay YALCIN <yalcinberkaycom@gmail.com>
2021-03-10 10:50:22 +0300

    Add Notification API

---
+++ Hermes.API.Notification/Controllers/HomeController.cs
@@ -0,0 +1,16 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Hermes.API.Notification.Controllers
+{
+    [ApiController]
+    [Route("v1/[controller]")]
+    public class HomeController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            return Ok("Notification Works");
+        }
+    }
+}
