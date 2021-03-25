b9d8f8b0f7de8e8005280cbf031ab00730ada458
Berkay YALCIN <yalcinberkaycom@gmail.com>
2021-03-25 15:26:19 +0300

    Add start-container shell script

---
+++ start-container.sh
@@ -0,0 +1,8 @@
+sudo docker pull yalcinberkay/hermes-advertisement-api
+sudo docker pull yalcinberkay/hermes-media-api
+sudo docker pull yalcinberkay/hermes-user-api
+sudo docker pull yalcinberkay/hermes-api-gateway
+sudo docker pull yalcinberkay/hermes-catalog-api
+sudo docker pull yalcinberkay/hermes-location-api
+sudo docker pull yalcinberkay/hermes-notification-api
+sudo /usr/local/bin/docker-compose up
