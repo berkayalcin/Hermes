3d6bbd137a0c725d0a5039db289f94c84d3b6548
Berkay Yalçın <berkay.yalcin@trendyol.com>
2021-05-03 22:53:03 +0300

    Add Initial Categories

--- Hermes.API.Catalog/Domain/EntityConfigurations/CategoryEntityConfiguration.cs
+++ Hermes.API.Catalog/Domain/EntityConfigurations/CategoryEntityConfiguration.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using Hermes.API.Catalog.Domain.Entities;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
@@ -27,6 +28,77 @@ namespace Hermes.API.Catalog.Domain.EntityConfigurations
             builder.Property(p => p.IsDeleted)
                 .HasDefaultValueSql("0")
                 .IsRequired();
+            builder.HasData(GetInitialCategories());
+        }
+
+        private List<Category> GetInitialCategories()
+        {
+            return new()
+            {
+                new Category
+                {
+                    Id = 1,
+                    Name = "Elektronik",
+                    Description = "Elektronik Eşyalar",
+                    Slug = "electronic",
+                    IsDeleted = false,
+                    ImageUrl = "https://hermes-application-images.s3.eu-central-1.amazonaws.com/laptop.png"
+                },
+                new Category
+                {
+                    Id = 2,
+                    Name = "Spor & Outdoor",
+                    Description = "Spor Outdoor Eşyaları",
+                    Slug = "outdoor",
+                    IsDeleted = false,
+                    ImageUrl = "https://hermes-application-images.s3.eu-central-1.amazonaws.com/camp.png"
+                },
+                new Category
+                {
+                    Id = 3,
+                    Name = "Araç",
+                    Description = "Motorsiklet, Bisiklet ve Arabalar",
+                    Slug = "vehicles",
+                    IsDeleted = false,
+                    ImageUrl = "https://hermes-application-images.s3.eu-central-1.amazonaws.com/vehicle.png"
+                },
+                new Category
+                {
+                    Id = 4,
+                    Name = "Ev & Bahçe",
+                    Description = "Ev & Bahçe Eşyaları",
+                    Slug = "garden",
+                    IsDeleted = false,
+                    ImageUrl = "https://hermes-application-images.s3.eu-central-1.amazonaws.com/garden.png"
+                },
+                new Category
+                {
+                    Id = 5,
+                    Name = "Moda & Aksesuarlar",
+                    Description = "Moda ve Aksesuar Eşyaları",
+                    Slug = "fashion-and-accessories",
+                    IsDeleted = false,
+                    ImageUrl = "https://hermes-application-images.s3.eu-central-1.amazonaws.com/fashion.png"
+                },
+                new Category
+                {
+                    Id = 6,
+                    Name = "Film & Kitap",
+                    Description = "Film & Kitap Eşyaları",
+                    Slug = "movies-and-books",
+                    IsDeleted = false,
+                    ImageUrl = "https://hermes-application-images.s3.eu-central-1.amazonaws.com/book.png"
+                },
+                new Category
+                {
+                    Id = 7,
+                    Name = "Diğer",
+                    Description = "Diğer Eşyalar",
+                    Slug = "others",
+                    IsDeleted = false,
+                    ImageUrl = "https://hermes-application-images.s3.eu-central-1.amazonaws.com/other.png"
+                },
+            };
         }
     }
 }
